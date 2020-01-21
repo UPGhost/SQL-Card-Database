d14d0e5b54ea00269d9cbf2a1ac3574f641df905
UPGhost <32511466+UPGhost@users.noreply.github.com>
2020-01-21 01:16:30 +0000

    21/01/2020 - Inheritance
    Follower and Spell Cards inherit from Full Card
    
    Champion Card inherits from Follower Card

---
+++ StarterApp/Old Scripts/FollowerCard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterApp
+{
+    public class FollowerCard : OldCard
+    {
+        public int AttackRating { get; set; }
+        public int HealthRating { get; set; }
+        public string Attribute1 { get; set; }
+        public string Attribute2 { get; set; }
+
+        public FollowerCard()
+        {
+            AttackRating = 0;
+            HealthRating = 0;
+            Attribute1 = "None";
+            Attribute2 = "None";
+        }
+            
+    }
+}
